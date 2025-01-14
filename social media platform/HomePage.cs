880ecc8003e96a238c356428ac83524cacddaa8f
Peter Emad <139802433+Peter-Emad100@users.noreply.github.com>
2025-01-14 16:52:03 +0200

    part1 show posts for user
    methods that will be used by showing posts method

---
+++ social media platform/HomePage.cs
@@ -0,0 +1,102 @@
+﻿using social_media_platform.data;
+using social_media_platform.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_media_platform
+{
+    internal class HomePage
+    {
+        internal AppDbContext _appDbContext;
+        public HomePage(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        List<long> followedUsersIds;
+        private List<long> GetListOfFollowedUsers(User user)
+        {
+            var followedUsers = _appDbContext.followedUsers
+                .Where(f => f.FollowerId == user.UserId)
+                .Select(f => f.FollowedId)
+                .ToList();
+            return followedUsers;
+        }
+        public bool showMultiPosts(User user)
+        {
+            followedUsersIds ??= GetListOfFollowedUsers(user);
+            int i = followedUsersIds.Count();
+            long postId;
+            while (i > 0)
+            {
+                showPost(i, out postId);
+                i--;
+            }
+            return true;
+
+        }
+        public int showPost(long FollowedUserId, out long postId)
+        {
+
+            var post = _appDbContext.posts.LastOrDefault(p => p.UserId == FollowedUserId);
+            Console.WriteLine($"{post.FirstName} {post.LastName} \n {post.Content} " +
+                $"\n  comments: \n {post.Comments}");
+            postId = post.PostId;
+            return 1;
+        }
+        public void ShowMultiComments(List<Comment> comments)
+        {
+            if (comments.Count() > 0)
+            {
+                for (int i = 0; i < comments.Count; i++)
+                {
+                    long commentId =ShowComment(comments[i]);
+                    Console.WriteLine("0 for next comment \n  1 for change comment (if it is yours)" +
+                        "2 for delete comment (if it is yours)");
+                    int choice;
+                    bool sucess =int.TryParse(Console.ReadLine(),out choice);
+                    if (sucess)
+                    {
+                        if (choice == 0)
+                        {
+                            continue;
+                        }
+                        else if (choice == 1){
+
+                        }
+                        else if (choice == 2)
+                        {
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("sorry you input was incorrect")
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("sorry you input was incorrect");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("there is no comments on this post, be the first and leave your comment");
+            }
+        }
+        public long ShowComment(Comment comment)
+        {
+            try
+            {
+                Console.WriteLine($"{comment.FirstName} {comment.LastName} \n " +
+                    $"{comment.Content}\n {comment.CommentCreationTime}");
+                return comment.CommentId;
+            }
+            catch { return -1; }
+        }
+    }
+}
--- social media platform/PostServices.cs
+++ social media platform/PostServices.cs
@@ -15,51 +15,54 @@ namespace social_media_platform
         {
             _appDbContext = appDbContext;
         }
-        public Post addPost(User user)
+        public Post AddPost(User user)
         {
             string content;
             Post post;
-            do {
+            do
+            {
                 Console.WriteLine("What's on your mind ?");
                 content = Console.ReadLine();
             }
             while (string.IsNullOrEmpty(content));
-                post = new Post() { UserId = user.UserId, Content = content, FirstName = user.FirstName, LastName = user.LastName, PostCreationTime = DateTimeOffset.UtcNow };
-                _appDbContext.Add(post);
-                _appDbContext.SaveChanges();
-            
+            post = new Post() { UserId = user.UserId, Content = content, FirstName = user.FirstName, LastName = user.LastName, PostCreationTime = DateTimeOffset.UtcNow };
+            _appDbContext.Add(post);
+            _appDbContext.SaveChanges();
+
             return post;
         }
-        public bool removePost(User user,long id) {
-            if (user == null || id<= 0) return false;
-                Post post = _appDbContext.posts.FirstOrDefault(p => p.PostId == id);
-                if (post == null) return false;
-                if (post.UserId == user.UserId)
-                {
-                    _appDbContext.Remove(post);
-                    _appDbContext.SaveChanges();
-                    return true;
-                }
-                return false;
-            
+        public bool RemovePost(User user, long id)
+        {
+            if (user == null || id <= 0) return false;
+            Post post = _appDbContext.posts.FirstOrDefault(p => p.PostId == id);
+            if (post == null) return false;
+            if (post.UserId == user.UserId)
+            {
+                _appDbContext.Remove(post);
+                _appDbContext.SaveChanges();
+                return true;
+            }
+            return false;
+
         }
-        public bool editPost(User user, long id)
+        public bool EditPost(User user, long id)
         {
             if (user == null || id <= 0) { return false; }
-                Post post = _appDbContext.posts.FirstOrDefault(p => p.PostId == id);
-                if (post == null) return false;
-                String content;
-                if (post.UserId == user.UserId)
-                {
-                    Console.WriteLine("Enter your new post content");
-                    content = Console.ReadLine();
-                    if (string.IsNullOrEmpty(content)) return false;
-                    post.Content = content;
-                    _appDbContext.SaveChanges();
-                    return true;
-                
+            Post post = _appDbContext.posts.FirstOrDefault(p => p.PostId == id);
+            if (post == null) return false;
+            String content;
+            if (post.UserId == user.UserId)
+            {
+                Console.WriteLine("Enter your new post content");
+                content = Console.ReadLine();
+                if (string.IsNullOrEmpty(content)) return false;
+                post.Content = content;
+                _appDbContext.SaveChanges();
+                return true;
+
             }
             return false;
         }
+
     }
 }
